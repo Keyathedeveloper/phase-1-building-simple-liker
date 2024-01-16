6380790faafd7cb20c044aed08f98474fb71a124
TaKeya McFadden <takeyamcfadden@gmail.com>
2024-01-16 18:33:22 -0500

    lab complete

--- main.js
+++ main.js
@@ -3,7 +3,59 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("errorModal");
 
+  errorModal.classList.add("hidden");
+
+  const handleServerResponse = () => {
+    mimicServerCall()
+    .then(() => {
+      toggleHearts();
+    })
+    .catch((error) => {
+
+      displayError(error);
+    });
+  };
+
+const toggleHearts = () => {
+  const hearts = document.querySelectorAll(".like-glyph");
+  hearts.forEach(heart) => {
+    if (heart.innerText == EMPTY_HEART) {
+      heart.innerText = FULL_HEART;
+      heart.classList.add("activated-heart");
+    } else {
+      heart.innerText = EMPTY_HEART;
+      heart.classList = FULL_HEART;
+    }
+  }
+}
+
+ const displayError = (error) => {
+    errorModal.classList.remove("hidden");
+
+  const errorMessage = document.getElementById("modal-message");
+    errorMessage.innerText = error;
+
+     setTimeout(() => {
+      errorModal.classList.add("hidden");
+    }, 3000);
+  };
+
+const emptyHearts = document.querySelectorAll(".like-glyph");
+  emptyHearts.forEach((heart) => {
+    heart.addEventListener("click", handleServerResponse);
+  });
+
+  const fullHearts = document.querySelectorAll(".activated-heart");
+  fullHearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      heart.innerText = EMPTY_HEART;
+      heart.classList.remove("activated-heart");
+    });
+  });
+}
 
 
 
